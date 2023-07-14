013d3ffeaedb318cd3a1d8ce6c32f610dede0079
dariofusco <fdario@hotmail.it>
2023-07-14 14:57:48 +0200

    crea array di oggetti

---
+++ js/main.js
@@ -0,0 +1,56 @@
+/* MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.*/
+
+const OurTeam = [
+    {
+        firstName: "Wayne",
+        lastName: "Barnett",
+        role: "Founder & CEO",
+        photo: "wayne - barnett - founder - ceo.jpg",
+    },
+
+    {
+        firstName: "Angela",
+        lastName: "Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        firstName: "Walter",
+        lastName: "Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+
+    {
+        firstName: "Angela",
+        lastName: "Lopez",
+        role: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        firstName: "Scott",
+        lastName: "Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+
+    {
+        firstName: "Barbara",
+        lastName: "Ramos",
+        role: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
+    },
+];
+
+/*MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.*/
+
+console.log(OurTeam)
+
+
+
+
+
